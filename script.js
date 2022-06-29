c945844563d98d1d60b11f48627e86ae02db9913
changothy <timothychang.au@gmail.com>
2022-06-29 17:14:36 -0500

    Add rules for displaying inputs

--- script.js
+++ script.js
@@ -35,9 +35,32 @@ function operate(operator, a, b) {
 }
 
 function populateDisplay(value) {
-    
+    displayContent = document.querySelector("#display").textContent;
+
+    if (value == "0" && displayContent == "0") {
+        return;
+    } else if (displayContent == "0" && value != "decimal") {
+        document.querySelector("#display").textContent = value;
+        return;
+    } else if (value == "decimal" && displayContent.includes("decimal")) { /* TODO: change decimal to . */
+        return;
+    } else {
+        document.querySelector("#display").textContent += value;
+    }
 }
 
 
-let displayValue = 0;
-document.querySelector("#display").textContent = displayValue;
+function initialize() {
+    let displayValue = 0;
+    document.querySelector("#display").textContent = displayValue;
+
+    buttons = document.querySelectorAll(".button");
+    console.log(buttons);
+    buttons.forEach(function (button) {
+        button.addEventListener('mouseup', () => {
+            populateDisplay(button.id);
+        });
+    });
+}
+
+initialize();
