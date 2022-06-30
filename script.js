773d6ec95207d4363385cf5273addeaee2dcd351
changothy <timothychang.au@gmail.com>
2022-06-30 16:50:50 -0500

    Refactor code to take in two values only

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let calculated = false;     // equation is calculated once equals is clicked
-let equation = new Array(); // stores the complete math equation
+let firstValue = null;
+let secondValue = null;
+let operator = null;
+
+// let equation = new Array(); // stores the complete math equation
 
 function add(a, b) {
     return a + b;
@@ -19,19 +23,32 @@ function divide(a, b) {
 
 // Operate on two digits based on operator passed in
 function operate(operator, a, b) {
+    let result = 0;
+
+    console.log(a);
+    console.log(b);
+    console.log(operator);
+
     switch(operator) {
         case "add":
-            return add(a, b);
+            result = add(a, b);
+            break;
         case "subtract":
-            return subtract(a, b);
+            result = subtract(a, b);
+            break;
         case "multiply":
-            return multiply(a, b);
-        case divide:
-            return divide(a, b);
+            result = multiply(a, b);
+            break;
+        case "divide":
+            result = divide(a, b);
+            break;
         default:
             console.log("Unrecognized operation");
             break;
     }
+    
+    document.querySelector("#display").textContent = result;
+    firstValue = result;
 }
 
 function isOperator(value) {
@@ -42,42 +59,45 @@ function isOperator(value) {
     }
 }
 
-// Calculate an equation
-function calculate(equation) {
-    const initialValue = 1;
-
-    let latestValue = equation[0];
-    let operator = "";
-
-    for (i = 1; i < equation.length; i++) {
-        if (isOperator(equation[i])) {
-            operator = equation[i];
-        } else if (!isNaN(equation[i])) {
-            latestValue = operate(operator, latestValue, equation[i]);
-        }
-    }
-
-    document.querySelector("#display").textContent = latestValue;
-    calculated = true;
+function reset() {
+    calculated = false;     // equation is calculated once equals is clicked
+    firstValue = null;
+    secondValue = null;
+    operator = null;
+    document.querySelector("#display").textContent = 0;
 }
+
+// // Calculate an equation
+// function calculate(equation) {
+//     const initialValue = 1;
+
+//     let latestValue = equation[0];
+//     let operator = "";
+
+//     for (i = 1; i < equation.length; i++) {
+//         if (isOperator(equation[i])) {
+//             operator = equation[i];
+//         } else if (!isNaN(equation[i])) {
+//             latestValue = operate(operator, latestValue, equation[i]);
+//         }
+//     }
+
+//     document.querySelector("#display").textContent = latestValue;
+//     calculated = true;
+// }
 
 // Updates the number display
 function populateDisplay(value) {
     displayContent = document.querySelector("#display").textContent;
-    let hasOperator = false;
-
-    if (displayContent.includes("x") || displayContent.includes("\\") || displayContent.includes("+") || displayContent.includes("-")) {
-        hasOperator = true;
-    }
 
     if (value == "0" && displayContent == "0") { // if display is 0, and 0 is clicked, don't update and add more 0s
         return;
     } else if (displayContent == "0" && !(value == "decimal" || isOperator(value))) { // if display is 0 and it's not a decimal or operator, then replace the default 0 with the value
         document.querySelector("#display").textContent = value;
         return;
-    } else if (value == "decimal" && displayContent.includes("decimal")) { // if there's already a decimal in the display value, return
+    } else if (value == "decimal" && displayContent.includes(".")) { // if there's already a decimal in the display value, return
         return;
-    } else if (!isNaN(value) && hasOperator) { // if value is a number and there's an operator, then update display with the new number
+    } else if (!isNaN(value) && operator) { // if value is a number and there's an operator, then update display with the new number
         document.querySelector("#display").textContent = value;
         return;
     } else {
@@ -86,10 +106,28 @@ function populateDisplay(value) {
 }
 
 // Update the equation array
-function recordElements(operator, equation) {
-    equation.push(document.querySelector("#display").textContent)
-    equation.push(operator);
-    console.log(equation);
+function recordElements(value) {
+    // equation.push(document.querySelector("#display").textContent)
+    // equation.push(operator);
+    // console.log(equation);
+    
+    if (firstValue && secondValue) {
+        // if (operator) {
+        //     operate(firstValue, secondValue, operator);
+        // }
+        operate(value, firstValue, secondValue);
+    } else if (firstValue) {
+        secondValue = Number(document.querySelector("#display").textContent);
+        console.log()
+        operate(operator, firstValue, secondValue);
+    } else {    // if number then operator, save the number and the operator
+        firstValue = Number(document.querySelector("#display").textContent);
+        operator = value;
+    }
+
+    // console.log(firstValue);
+    // console.log(secondValue);
+    // console.log(operator);
 }
 
 function initialize() {
@@ -98,11 +136,11 @@ function initialize() {
     numberButtons = document.querySelectorAll(".button.number");
     numberButtons.forEach(function (button) {
         button.addEventListener('mouseup', () => {
-            if (calculated) {
-                equation = new Array();
-                document.querySelector("#display").textContent = 0;
-                calculated = false;
-            }
+            // if (calculated) {
+            //     equation = new Array();
+            //     document.querySelector("#display").textContent = 0;
+            //     calculated = false;
+            // }
             populateDisplay(button.textContent);
         });
     });
@@ -110,25 +148,26 @@ function initialize() {
     operationButtons = document.querySelectorAll("#operators .button");
     operationButtons.forEach(function (button) {
         button.addEventListener('mouseup', () => {
-            if (calculated) {
-                equation = new Array();
-                calculated = false;
-            }
-            recordElements(button.id, equation);
-            populateDisplay(button.textContent);
+            // if (calculated) {
+            //     equation = new Array();
+            //     calculated = false;
+            // }
+            recordElements(button.id);
+            // populateDisplay(button.textContent);
         });
     });
 
     clearButton = document.querySelector("#clear");
     clearButton.addEventListener('mouseup', () => {
-        equation = new Array();
+        // equation = new Array();
         document.querySelector("#display").textContent = 0;
+        reset();
     });
 
     equalsButton = document.querySelector("#equals");
     equalsButton.addEventListener('mouseup', () => {
-        recordElements(equalsButton.id, equation);
-        calculate(equation);
+        recordElements(equalsButton.id);
+        // calculate(equation);
     });
 }
 
